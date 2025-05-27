1c885b3928e9bba2bb3b63a32b7a2923e5e15b60
slayer0x10 <saicharanruka@gmail.com>
2025-05-27 17:42:54 +0530

    added new section

--- src/sections/Workouts.tsx
+++ src/sections/Workouts.tsx
@@ -2,11 +2,12 @@ import React from "react";
 
 const Workouts = () => {
 	return (
-		<section className="pt-24">
+		<section className="pt-24 pb-[1000px]">
 			<div className="flex flex-col justify-center items-center gap-2">
-				<h1>Find your perfect workout plan</h1>
-				<p>
-					{" "}
+				<h1 className="text-4xl font-medium text-center">
+					Find your perfect workout plan
+				</h1>
+				<p className="opacity-50 text-center max-w-2xl font-semibold tracking-tighter">
 					We believe that convenience, expert guidance, top-tier resources, and
 					a supportive environment are the pillars of a successful fitness
 					journey. That&apos;s why we offer:
